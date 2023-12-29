e94c3ec5912b09c133a9bee6f2ddd51891e37287
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-12-30 01:26:19 +0300

    Update siralama_algoritmalari.py

--- siralama_algoritmalari.py
+++ siralama_algoritmalari.py
@@ -103,36 +103,106 @@ sayilar = [6, 8, 5, 1, 9, 10, -5, 99, 13]
 kabarcik_siralama(sayilar)
 print(sayilar)   
 
+"""
+##################### 3) Hızlı Sıralama (Quick Sort) ##########################
+
+Bir pivot seçilir, sağ sol ya da ortadan. Sağındaki elemanlar pivottan büyük, solundakiler küçük olmalı.
+Daha sonra kalan kısımlar kendi aralarında sıralanmalı.
+
+- t değeri pivottan büyük olunca bir sonrakine geç, küçük olunca i'yi 1 arttır ve t'yi sabit tut.
+- Gezme işlemi bittikten sonra parçalamaya başlarız ve parçalamaya (i+1)'den başlarız.
+"""
+
+def parcala(dizi, low, high):
+    
+    if high > low:
+        pivot = dizi[high]
+        i = low - 1
+        
+        for t in range(low, high):
+            if dizi[t] < pivot:
+                i += 1
+                dizi[i], dizi[t] = dizi[t], dizi[i]
+        
+        dizi[i+1], dizi[high] = dizi[high], dizi[i+1]
+        return i+1
+
+def hizli_siralama(dizi, low, high):
+    
+    if high > low:
+        parcalama_indeksi = parcala(dizi, low, high)
+        
+        # İndeksin solunda kalanları sırala
+        hizli_siralama(dizi, low, parcalama_indeksi-1)
+    
+        # İndeksin sağında kalanları sırala
+        hizli_siralama(dizi, parcalama_indeksi+1, high)
+    
+yeni_liste = [11, 9, 16, 5, 10]
+hizli_siralama(yeni_liste, 0, len(yeni_liste)-1)
+print(yeni_liste)
 
 
+"""
+##################### 3) Eklemeli Sıralama (Insertion Sort) ###################
+"""
+        
+def eklemeli_siralama(dizi):
+    
+    for t in range(1, len(dizi)):
+        deger = dizi[t]
+        x = t - 1
+        
+        while x >= 0 and deger < dizi[x]:
+            dizi[x+1] = dizi[x]
+            x -= 1
+        
+        dizi[x+1] = deger
+            
+            
+liste2 = [11, 2, 5, 7, 8, 4, 1, -6]
+eklemeli_siralama(liste2)
+print(liste2)            
 
 
+"""
+##################### 4) Birleştirmeli Sıralama (Merge Sort) ###################
+"""
 
-
-
-
-
-
-
-
-
-
-
-
+def birlestirmeli_siralama(dizi):
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    if len(dizi) > 1:
+        orta_nokta = len(dizi) // 2 # tam sayılı bölme
+        sol = dizi[:orta_nokta]
+        sag = dizi[orta_nokta:]
+        
+        birlestirmeli_siralama(sol)
+        birlestirmeli_siralama(sag)
+        
+        i = k = j = 0 # i, j, k = 0, 0, 0
+        
+        while i < len(sol) and j < len(sag):
+            if sol[i] < sag[j]:
+                dizi[k] = sol[i]
+                i += 1
+            else:
+                dizi[k] = sag[j]
+                j += 1
+            
+            k += 1
+        
+        # Solda eleman olduğu sürece
+        while i < len(sol):
+            dizi[k] = sol[i]
+            i += 1
+            k += 1
+        
+        # Sağda eleman olduğu sürece
+        while j < len(sag):
+            dizi[k] = sag[j]
+            j += 1
+            k += 1
+            
+liste3 = [11, 2, 5, 7, 8, 4, 1, -6]
+birlestirmeli_siralama(liste3)
+print(liste3)       
